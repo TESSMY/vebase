58c4cd9eac462a387d639aae01a2942a3643580f
Marcus Loo <marcuslooek@gmail.com>
2023-02-20 17:46:09 +0800

    - added relatable, sortable, create, and update - updated vecontroller

--- src/Http/Controllers/VeController.php
+++ src/Http/Controllers/VeController.php
@@ -7,80 +7,184 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class VeController extends Controller
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     protected $model;
+    protected $tableName;
+    protected $userType;
 
     /**
      * creates the model from the request path
      */
     public function __construct(Request $request)
     {
-        $class = \Illuminate\Support\Str::singular($request->segment(2));
-        $this->model = app('App\\Models\\' . ucfirst($class));  
+        $this->tableName = $request->segment(2);
+
+        $class = Str::singular($request->segment(2));
+        $this->model = app('App\\Models\\' . ucfirst($class));
+
+        if (Auth::user()->canAccessAdmin) {
+            $this->userType = 'admin';
+        }
     }
 
     public function index(Request $request)
     {
         $search = $request->input('search');
+        $orderColumn = $request->input('order_column');
+        $orderBy = $request->input('order_by');
 
-        $model = $this->model::query();
+        $models = $this->model::query();
 
         if (!empty($search)) {
-            if (empty($model::searchable)) {
-                throw new Exception("Model searchable is empty");
-            } else {
-                foreach ($model::searchable as $key => $value) {
-                    $model = $model->where(function($query) use ($search, $value) {
-                        $query->where($value, 'LIKE', '%' . $search . '%')
-                            ->orWhere('email', 'LIKE', '%' . $search . '%');
+            if (empty($this->model::searchable)) {
+                throw new Exception($this->model . " searchable is empty");
+            }
+
+            foreach ($this->model::searchable as $key => $value) {
+                if (!Schema::hasColumn($this->tableName, $value)) {
+                    throw new Exception($value . " column does not exist on " . $this->model . ' table');
+                } else {
+                    $models = $models->where(function($query) use ($search, $key, $value) {
+                        if ($key == 0) {
+                            $query->where($value, 'LIKE', '%' . $search . '%');
+                        } else {
+                            $query->orWhere($value, 'LIKE', '%' . $search . '%');
+                        }
                     });
                 }
             }
         }
 
+        if (!empty($orderColumn)) {
+            if (empty($this->model::sortable)) {
+                throw new Exception($this->model . " sortable is empty");
+            }
+            if (!in_array($orderColumn, $this->model::sortable)) {
+                throw new Exception($orderColumn . " is not in " . $this->model . " sortable");
+            }
+
+            $models = $models->orderBy($orderColumn, $orderBy);
+        }
+
+        if (!empty($this->model::relatable)) {
+            $models->load($this->model::relatable);
+        }
+
+        // need to check if orderby and sortby will work together
         $sortBy = $request->input('sort_by', 'latest');
         if($sortBy === 'oldest'){
-            $model->oldest();
+            $models->oldest();
         } elseif($sortBy === 'latest'){
-            $model->latest();
+            $models->latest();
         }
+
+        return view($this->userType . '.' . $this->tableName . '.index', compact('models'));
     }
 
-    public function create()
+    public function create(Request $request)
     {
-        // 
+        return view($this->userType . '.' . $this->tableName . '.create');
     }
 
     public function store(Request $request)
     {
-        // 
+        $input = $request->all();
+
+        if (empty($this->model::create)) {
+            throw new Exception($this->model . " create is empty");
+        }
+
+        $validator = Validator::make($input, $model::create);
+        if ($validator->fails()) {
+            foreach ($validator->errors()->all() as $error) {
+                flash('Error: ' . $error)->error();
+            }
+            return back()->withInput($request->input())->withErrors($validator);
+        }
+
+        try {
+            DB::beginTransaction();
+
+            $model::create($input);
+
+            DB::commit();
+            flash()->success('Successfully create ' .  $model);
+            return redirect()->route($this->userType . '.' . $this->tableName . '.index');
+        } catch (Exception $exception) {
+            DB::rollBack();
+            Log::error($exception);
+            flash()->error('There was an error creating ' . $model);
+            return back()->withInput();
+        }
     }
 
     public function show(Request $request, $id)
     {
         $model = $this->model::find($id);
         abort_if(empty($model),404);
+
+        if (!empty($model::relatable)) {
+            $model->load($model::relatable);
+        }
         
-        dd($model->toArray());
+        return view($this->userType . '.' . $this->tableName . '.show', compact('model'));
     }
 
     public function edit(Request $request, $id)
     {
-        // 
+        $model = $this->model::find($id);
+        abort_if(empty($model),404);
+
+        return view($this->userType . '.' . $this->tableName . '.edit', compact('model'));
     }
 
     public function update(Request $request, $id)
     {
-        // 
+        $model = $this->model::find($id);
+        abort_if(empty($model),404);
+
+        $input = $request->all();
+
+        if (empty($this->model::create)) {
+            throw new Exception($this->model . " create is empty");
+        }
+
+        $validator = Validator::make($input, $model::update);
+        if ($validator->fails()) {
+            foreach ($validator->errors()->all() as $error) {
+                flash('Error: ' . $error)->error();
+            }
+            return back()->withInput($request->input())->withErrors($validator);
+        }
+
+        try {
+            DB::beginTransaction();
+
+            $model::update($input);
+
+            DB::commit();
+            flash()->success('Successfully create ' .  $model);
+            return redirect()->route($this->userType . '.' . $this->tableName . '.index');
+        } catch (Exception $exception) {
+            DB::rollBack();
+            Log::error($exception);
+            flash()->error('There was an error creating ' . $model);
+            return back()->withInput();
+        }
     }
 
     public function destroy($id)
     {
-        // 
+        $model = $this->model::find($id);
+        abort_if(empty($model),404);
+
+        flash()->success('Successfully deleted ' . $model);
+        return redirect()->route($this->userType . '.' . $this->tableName . '.index');
     }
 
 }
--- src/Traits/VeModel.php
+++ src/Traits/VeModel.php
@@ -20,6 +20,35 @@ trait VeModel
      */
     protected array $files = [];
 
+    /**
+     * The relations that should be loaded in the `index` or `show` functions
+     * Make sure to add the relation in the model before adding it here or the relatable will not work
+     * @var array
+     */
+    protected array $relatable = [];
+
+    /**
+     * The fields that can be sorted in asc or desc in the `index` functions
+     * @var array
+     */
+    protected array $sortable = [];
+
+    /**
+     * The fields that are needed during creation
+     * E.g. 'email' => 'required|unique:users,email,' . $this->id . ',id,deleted_at,NULL',
+     *
+     * @var array
+     */
+    protected array $create = [];
+
+    /**
+     * The fields that are needed during update
+     * E.g. 'name' => 'required|min:3'
+     *
+     * @var array
+     */
+    protected array $update = [];
+
     abstract public function hasApiResourceRoute() : bool;
 
     abstract public function hasAdminResourceRoute() : bool;

f039122a55aa591fcfce19a9e2b099fa7546fbd1
MichelleKong <michelle5090@gmail.com>
2023-02-21 10:35:18 +0800

    product & product bundle UI

---
+++ resources/views/product/products.blade.php
@@ -0,0 +1,47 @@
+<div class="table-responsive">
+    <table class="table table-rounded table-striped shadow w-100 mt-4">
+        <thead>
+            <tr class="text-left font-bold">
+                <th class="pb-4 pt-6 px-6">Product</th>
+                <th class="pb-4 pt-6 px-6">Sku</th>
+                <th class="pb-4 pt-6 px-6">Supplier</th>
+                <th class="pb-4 pt-6 px-6">Brand</th>
+                <th class="pb-4 pt-6 px-6">Piece Per Carton</th>
+                <th class="pb-4 pt-6 px-6">Total Stock</th>
+                <th class="pb-4 pt-6 px-6">Available Stock</th>
+                <th class="pb-4 pt-6 px-6">Sales Order Stock</th>
+                <th class="pb-4 pt-6 px-6">Retail Selling Price</th>
+                <th class="pb-4 pt-6 px-6">Unit Cost</th>
+                <th class="pb-4 pt-6 px-6">Lead Time</th>
+                <th class="pb-4 pt-6 px-6">Safety Stock</th>
+                <th class="pb-4 pt-6 px-6">Updated at</th>
+                <th class="pb-4 pt-6 px-6">Status</th>
+                <th class="pb-4 pt-6 px-6">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    <div class="d-inline-flex">
+                        <img src="" width="50" height="50" />
+                        <span class="py-3 px-2 ">Adirondack Chair</span>
+                    </div>
+                </td>
+                <td>SKU #12345</td>
+                <td>Supplier #12345</td>
+                <td>Brand Name</td>
+                <td>15</td>
+                <td>150</td>
+                <td>150</td>
+                <td>150</td>
+                <td>$45.20</td>
+                <td>$36.20</td>
+                <td>3 Days</td>
+                <td>50</td>
+                <td>10/02/2023 15:35 PM</td>
+                <td><button class="btn btn-success btn-sm w-100">Active</button></td>
+                <td><a href="#" class="btn btn-alt px-5">View</a></PrimaryButton></td>
+            </tr>
+        </tbody>
+    </table>
+</div>

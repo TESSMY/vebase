7ed8897b02fa8abc43a763f28f8a43f8387dfb2b
MichelleKong <michelle5090@gmail.com>
2023-02-21 10:52:00 +0800

    product & product bundle ui

---
+++ resources/views/product/overview.blade.php
@@ -0,0 +1,65 @@
+<div class="row mt-5">
+    <div class="col-md-5">
+        <div>
+            <img src="g" width="500" height="500" />
+        </div>
+    </div>
+    <div class="col-md-7">
+        <div class="d-flex">
+            <div class="col-md-8 pe-6">
+                <h3>Amazing Modern Chair (Orange)</h3>
+                <small class="fw-light">Added Date: 10/10/2022.</small>
+            </div>
+            <div class="col-md-4">
+                <div>
+                    <a href="#" class="btn btn-alt px-5">Edit Product</a>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex mt-3">
+            <div class="col-md-6 pe-6">
+                <div>Unit Cost:</div>
+                <b>$25.90</b>
+            </div>
+            <div class="col-md-6">
+                <div>Retail Price:</div>
+                <b>$45.90</b>
+            </div>
+        </div>
+
+        <div class="mt-3">
+            <div>Description:</div>
+            <div>Lorem ipsum</div>
+        </div>
+
+        <div class="d-flex mt-3">
+            <div class="col-md-4 pe-6">
+                <div>SKU:</div>
+                <b>SKU #12345</b>
+            </div>
+            <div class="col-md-4 pe-6">
+                <div>Barcode:</div>
+                <b>#12345</b>
+            </div>
+            <div class="col-md-4">
+                <div>Location:</div>
+                <b>Warehouse, Changi</b>
+            </div>
+        </div>
+
+        <div class="d-flex mt-3">
+            <div class="col-md-4 pe-6">
+                <div>Available Stock:</div>
+                <b>1784</b>
+            </div>
+            <div class="col-md-4 pe-6">
+                <div>Supplier:</div>
+                <b>Supplier #12345</b>
+            </div>
+            <div class="col-md-4">
+                <div>Reorder Point:</div>
+                <b>75</b>
+            </div>
+        </div>
+    </div>
+</div>

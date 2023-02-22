69714c3e5fbe326f074e14ad61607757a20df41b
Marcus Loo <marcuslooek@gmail.com>
2023-02-22 17:53:46 +0800

    routekey handling

--- src/Http/Controllers/VeController.php
+++ src/Http/Controllers/VeController.php
@@ -7,6 +7,10 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 
 class VeController extends Controller
@@ -15,21 +19,34 @@ class VeController extends Controller
 
     protected $model;
     protected $tableName;
-    protected $userType;
+    protected $folder;
 
     /**
      * creates the model from the request path
      */
     public function __construct(Request $request)
     {
-        $this->tableName = $request->segment(2);
 
-        $class = Str::singular($request->segment(2));
-        $this->model = app('App\\Models\\' . ucfirst($class));
+        if (!empty($request->segments())) {
+            $this->tableName = $request->segment(2);
+            $class = Str::singular($request->segment(2));
+            $this->model = app('App\\Models\\' . ucfirst($class));
 
-        if (Auth::user()->canAccessAdmin) {
-            $this->userType = 'admin';
+            if (Auth::user()->canAccessAdmin()) {
+                $this->folder = 'admin';
+            }
+            $this->authorizeResource($this->model::class, $this->model);
         }
+    }
+
+    public function checkRouteKey($id) {
+        if ($this->model->getRouteKey() != 'id') {
+            $model = $this->model::where($this->model->getRouteKey(), '$id')->first();
+            abort_if(empty($model), 404);
+        } else {
+            $model = $this->model::findOrFail($id);
+        }
+        return $model;
     }
 
     public function index(Request $request)
@@ -42,30 +59,22 @@ class VeController extends Controller
 
         if (!empty($search)) {
             if (empty($this->model::searchable)) {
-                throw new Exception($this->model . " searchable is empty");
+                throw new \Exception($this->model . " searchable is empty");
             }
 
-            foreach ($this->model::searchable as $key => $value) {
-                if (!Schema::hasColumn($this->tableName, $value)) {
-                    throw new Exception($value . " column does not exist on " . $this->model . ' table');
-                } else {
-                    $models = $models->where(function($query) use ($search, $key, $value) {
-                        if ($key == 0) {
-                            $query->where($value, 'LIKE', '%' . $search . '%');
-                        } else {
-                            $query->orWhere($value, 'LIKE', '%' . $search . '%');
-                        }
-                    });
+            $models = $models->where(function($query) use ($search) {
+                foreach ($this->model::searchable as $value) {
+                    $query->orWhere($value, 'LIKE', '%' . $search . '%');
                 }
-            }
+            });
         }
 
         if (!empty($orderColumn)) {
             if (empty($this->model::sortable)) {
-                throw new Exception($this->model . " sortable is empty");
+                throw new \Exception($this->model . " sortable is empty");
             }
             if (!in_array($orderColumn, $this->model::sortable)) {
-                throw new Exception($orderColumn . " is not in " . $this->model . " sortable");
+                throw new \Exception($orderColumn . " is not in " . $this->model . " sortable");
             }
 
             $models = $models->orderBy($orderColumn, $orderBy);
@@ -77,18 +86,18 @@ class VeController extends Controller
 
         // need to check if orderby and sortby will work together
         $sortBy = $request->input('sort_by', 'latest');
-        if($sortBy === 'oldest'){
+        if ($sortBy === 'oldest'){
             $models->oldest();
-        } elseif($sortBy === 'latest'){
+        } elseif ($sortBy === 'latest'){
             $models->latest();
         }
 
-        return view($this->userType . '.' . $this->tableName . '.index', compact('models'));
+        return view($this->folder . '.' . $this->tableName . '.index', compact('models'));
     }
 
     public function create(Request $request)
     {
-        return view($this->userType . '.' . $this->tableName . '.create');
+        return view($this->folder . '.' . $this->tableName . '.create');
     }
 
     public function store(Request $request)
@@ -96,10 +105,10 @@ class VeController extends Controller
         $input = $request->all();
 
         if (empty($this->model::create)) {
-            throw new Exception($this->model . " create is empty");
+            throw new \Exception($this->model . " create is empty");
         }
 
-        $validator = Validator::make($input, $model::create);
+        $validator = Validator::make($input, $this->model::create);
         if ($validator->fails()) {
             foreach ($validator->errors()->all() as $error) {
                 flash('Error: ' . $error)->error();
@@ -110,48 +119,45 @@ class VeController extends Controller
         try {
             DB::beginTransaction();
 
-            $model::create($input);
+            $this->model::create($input);
 
             DB::commit();
-            flash()->success('Successfully create ' .  $model);
-            return redirect()->route($this->userType . '.' . $this->tableName . '.index');
-        } catch (Exception $exception) {
+            flash()->success('Successfully create ' .  $this->model);
+            return redirect()->route($this->folder . '.' . $this->tableName . '.index');
+        } catch (\Exception $exception) {
             DB::rollBack();
             Log::error($exception);
-            flash()->error('There was an error creating ' . $model);
+            flash()->error('There was an error creating ' . $this->model);
             return back()->withInput();
         }
     }
 
     public function show(Request $request, $id)
     {
-        $model = $this->model::find($id);
-        abort_if(empty($model),404);
+        $model = $this->checkRouteKey($id);
 
         if (!empty($model::relatable)) {
             $model->load($model::relatable);
         }
         
-        return view($this->userType . '.' . $this->tableName . '.show', compact('model'));
+        return view($this->folder. '.' . $this->tableName . '.show', compact('model'));
     }
 
     public function edit(Request $request, $id)
     {
-        $model = $this->model::find($id);
-        abort_if(empty($model),404);
+        $model = $this->checkRouteKey($id);
 
-        return view($this->userType . '.' . $this->tableName . '.edit', compact('model'));
+        return view($this->folder . '.' . $this->tableName . '.edit', compact('model'));
     }
 
     public function update(Request $request, $id)
     {
-        $model = $this->model::find($id);
-        abort_if(empty($model),404);
+        $model = $this->checkRouteKey($id);
 
         $input = $request->all();
 
         if (empty($this->model::create)) {
-            throw new Exception($this->model . " create is empty");
+            throw new \Exception($this->model . " create is empty");
         }
 
         $validator = Validator::make($input, $model::update);
@@ -168,23 +174,23 @@ class VeController extends Controller
             $model::update($input);
 
             DB::commit();
-            flash()->success('Successfully create ' .  $model);
-            return redirect()->route($this->userType . '.' . $this->tableName . '.index');
-        } catch (Exception $exception) {
+            flash()->success('Successfully create ' .  $this->model);
+            return redirect()->route($this->folder . '.' . $this->tableName . '.index');
+        } catch (\Exception $exception) {
             DB::rollBack();
             Log::error($exception);
-            flash()->error('There was an error creating ' . $model);
+            flash()->error('There was an error creating ' . $this->model);
             return back()->withInput();
         }
     }
 
     public function destroy($id)
     {
-        $model = $this->model::find($id);
-        abort_if(empty($model),404);
+        $model = $this->checkRouteKey($id);
+        $model->delete();
 
-        flash()->success('Successfully deleted ' . $model);
-        return redirect()->route($this->userType . '.' . $this->tableName . '.index');
+        flash()->success('Successfully deleted ' . $this->model);
+        return redirect()->route($this->folder . '.' . $this->tableName . '.index');
     }
 
 }

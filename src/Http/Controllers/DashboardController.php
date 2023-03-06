7c747aa2a185615c251a18b08323013a68a9e32c
Agung Kurniawan <upload.kurniawan@gmail.com>
2023-03-06 12:47:58 +0700

    top order

--- src/Http/Controllers/DashboardController.php
+++ src/Http/Controllers/DashboardController.php
@@ -3,6 +3,9 @@
 namespace Vecapital\Vebase\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\DeliveryOrder;
+use App\Models\SalesOrder;
+use App\Models\PurchaseOrder;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\View;
@@ -11,8 +14,8 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $purchaseOrderThisMonth = \App\Models\DeliveryOrder::whereMonth('created_at', date('m'))->sum('grant_total');
-        $purchaseOrderLastMonth = \App\Models\DeliveryOrder::whereMonth('created_at', date('m', strtotime('-1 month')))->sum('grant_total');
+        $purchaseOrderThisMonth = DeliveryOrder::whereMonth('created_at', date('m'))->sum('grant_total');
+        $purchaseOrderLastMonth = DeliveryOrder::whereMonth('created_at', now()->subMonth()->month)->sum('grant_total');
 
         $purchaseOrderStatics = [
             'total' => (float) $purchaseOrderThisMonth,
@@ -29,25 +32,24 @@ class DashboardController extends Controller
             'growth' => 1.4
         ];
 
-        $salesOrderThisMonth = \App\Models\SalesOrder::whereMonth('created_at', date('m'))->sum('grant_total');
-        $salesOrderLastMonth = \App\Models\SalesOrder::whereMonth('created_at', date('m', strtotime('-1 month')))->sum('grant_total');
+        $salesOrderThisMonth = SalesOrder::whereMonth('created_at', date('m'))->sum('grant_total');
+        $salesOrderLastMonth = SalesOrder::whereMonth('created_at', date('m', strtotime('-1 month')))->sum('grant_total');
         $salesOrderStatics = [
             'total' => (float) $salesOrderThisMonth,
             'growth' => $salesOrderLastMonth ? ($purchaseOrderThisMonth - $salesOrderThisMonth) / $salesOrderLastMonth : 100
         ];
 
-        $monthlyOrders = \App\Models\SalesOrder::select([DB::raw("SUM(grant_total) as total"), DB::raw("CONCAT(YEAR(created_at),MONTH(created_at)) as month")])
-            ->whereRaw(DB::raw('CONCAT(YEAR(created_at),MONTH(created_at)) >= ?'), date('Ym', strtotime('-1 year')))
-            ->orderBy('created_at')
-            ->groupBy('month')
+        $monthlyOrders = TopOrder::whereRaw(DB::raw('CONCAT(year, month) >= ?'), date('Ym', strtotime('-1 year')))
+            ->orderBy('year')
+            ->orderBy('month')
             ->get()
             ->map(function ($item) {
                 $item->total = (float) $item->total;
                 return $item;
             });
 
-        $purchaseOrders = \App\Models\PurchaseOrder::with('supplier')->limit(10)->orderBy('created_at', 'desc')->get();
-        $salesOrders = \App\Models\SalesOrder::limit(10)->orderBy('created_at', 'desc')->get();
+        $purchaseOrders = PurchaseOrder::with('supplier')->limit(10)->orderBy('created_at', 'desc')->get();
+        $salesOrders = SalesOrder::limit(10)->orderBy('created_at', 'desc')->get();
 
         return View::make('vebase::admin.dashboard', compact(
             'purchaseOrderStatics',

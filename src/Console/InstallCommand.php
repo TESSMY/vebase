0249a91c7b834ca86520d32aa6250e8238bf2ea7
Marcus Loo <marcuslooek@gmail.com>
2023-03-06 15:02:30 +0800

    move controller to resource root folder add install command

---
+++ src/Console/InstallCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Vecapital\Vebase\Console;
+
+use Illuminate\Console\Command;
+
+class InstallCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'vebase:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install the UI for vebase';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // views
+        $viewDirectories = scandir(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'views');
+        foreach ($viewDirectories as $viewDirectory) {
+            // skip '.' and '..' in dir
+            if (strlen($model) > 2) {
+                copy(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . $viewDirectory, resource_path('views/admin' . DIRECTORY_SEPARATOR . $viewDirectory));
+            }
+        }
+
+        // controller
+        $controllers = scandir(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'controllers');
+        foreach ($controllers as $controller) {
+            // skip '.' and '..' in dir
+            if (strlen($model) > 2) {
+                copy(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'controllers' . DIRECTORY_SEPARATOR . $controller, app_path('Http' . DIRECTORY_SEPARATOR . 'Controllers' . DIRECTORY_SEPARATOR .
+                'Admin' . DIRECTORY_SEPARATOR . 'Models' . DIRECTORY_SEPARATOR . $controller));
+            }
+        }
+
+        $this->info('Succesfully installed UI');
+    }
+}

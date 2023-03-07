9ba80584fec4d08dd44c1e1e0b20b44e31417607
Marcus Loo <marcuslooek@gmail.com>
2023-03-07 14:31:10 +0800

    invoice form and create view

---
+++ resources/components/InvoiceForm.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="row mb-2">
+        <span class="h4">ADD PRODUCT</span>
+    </div>
+    <div class="overflow-auto">
+        <table class="table table-bordered text-center">
+            <thead>
+                <tr>
+                    <th>Product Details</th>
+                    <th>Quantity</th>
+                    <th>Unit Price</th>
+                    <th>Sub Total</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item, index) in products">
+                    <td>Product Name</td>
+                    <td>
+                        <input class="form-control" type="number" min="0" v-model="item.quantity" @input="updateProductSubTotal(item)" required>
+                    </td>
+                    <td>{{ item.product.unit_price }}</td>
+                    <td>{{ item.subTotal }}</td>
+                    <td>
+                        <span class="btn" @click="removeProduct(index)">
+                            <i class="uil-trash" style="color: red"></i>
+                        </span>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="row container-fluid">
+        <div class="col-12 col-md-4 mb-md-0 mb-3">
+            <span class="btn" @click="addProducts()">Add another line</span>
+            <div class="row px-0">
+                <label class="form-label px-0">Notes and instructions</label>
+                <textarea class="form-control" placeholder="Notes and instructions" rows="5" style="resize: none"></textarea>
+            </div>
+        </div>
+        <div class="col-md-5"></div>
+        <div class="col-12 col-md-3">
+            <div class="row text-end">
+                <span class="col-8 fw-bold">Sub Total: </span>
+                <span class="col-4">{{ subTotal }}</span>
+                <div class="border my-2"></div>
+                <span class="col-8 fw-bold">Tax: </span>
+                <span class="col-4">15%</span>
+                <div class="border my-2"></div>
+                <span class="col-8 fw-bold">Total (SGD): </span>
+                <span class="col-4">{{ grandTotal }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const subTotal = ref(0);
+const grandTotal = ref(0);
+const products = ref([{
+    'product': {
+        unit_price: 1,
+    },
+    'quantity': 0,
+    'subTotal': 0,
+}])
+
+function addProducts() {
+    this.products.push({
+        'product': {
+            unit_price: 1,
+        },
+        'quantity': 0,
+        'subTotal': 0,
+    })
+}
+
+function removeProduct(index) {
+    this.products.splice(index, 1);
+}
+
+function updateProductSubTotal(item) {
+    item.subTotal = item.product.unit_price * item.quantity;
+    this.updateTotalPrice();
+}
+
+function updateTotalPrice() {
+    this.subTotal = 0;
+    this.grandTotal = 0;
+    this.products.forEach(item => {
+        this.subTotal += item.subTotal;
+        this.grandTotal += item.subTotal;
+    });
+}
+
+</script>
--- resources/views/invoices/create.blade.php
+++ resources/views/invoices/create.blade.php
@@ -6,7 +6,7 @@
         <div class="row">
             <div class="col-12 col-md-6">
                 <div class="page-title-box">
-                    <span class="page-title h4">Add New {{ $modelName }}</span>
+                    <span class="page-title h4">Create New {{ $modelName }}</span>
                 </div>
             </div>
             <nav class="col-12 col-md-6">
@@ -17,75 +17,22 @@
             </nav>
         </div>
         <div class="border my-2 mb-3"></div>
-        <div class="bg-white card shadow py-3 px-4">
-            <form action="{{ route($routePrefix . '.' . $routeName . '.store') }}" method="POST" enctype="multipart/form-data">
-                @csrf
+        <form action="{{ route($routePrefix . '.' . $routeName . '.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="bg-white card shadow py-3 px-4">
                 <div class="row border-bottom mb-2">
                     <span class="h5">{{ $modelName }} Information</span>
                 </div>
                 @include('admin.invoices.form')
+            </div>
+            <div class="border my-2 mb-3"></div>
+            <div class="bg-white card shadow py-3 px-4">
+                <invoice-form></invoice-form>
                 <div class="row col-12">
                     <button type="submit" class="col-12 col-md-1 btn btn-success m-2">Create</button>
                     <a href="{{ route($routePrefix . '.' . $routeName . '.index') }}" class="col-12 col-md-1 btn btn-dark m-2">Back</a>
                 </div>
-            </form>
-        </div>
-        <div class="border my-2 mb-3"></div>
-        <div class="bg-white card shadow py-3 px-4">
-            <div class="row mb-2">
-                <span class="h4">ADD PRODUCT</span>
             </div>
-            <div class="overflow-auto">
-                <table class="table table-bordered text-center">
-                    <thead>
-                        <tr>
-                            <th>Product Details</th>
-                            <th>Quantity</th>
-                            <th>Unit Price</th>
-                            <th>Total Amount</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Product Name</td>
-                            <td>
-                                <input class="form-control" type="number" min="0" required>
-                            </td>
-                            <td>Otto</td>
-                            <td>Otto</td>
-                            <td>
-                                <span class="btn">
-                                    <i class="uil-trash" style="color: red"></i>
-                                </span>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div class="row container-fluid">
-                <div class="col-12 col-md-4 mb-md-0 mb-3">
-                    <span>Add another line</span>
-                    <span>Import Product List</span>
-                    <div class="row px-0">
-                        <label class="form-label px-0">Notes and instructions</label>
-                        <textarea class="form-control" placeholder="Notes and instructions" rows="5" style="resize: none"></textarea>
-                    </div>
-                </div>
-                <div class="col-md-5"></div>
-                <div class="col-12 col-md-3">
-                    <div class="row text-end">
-                        <span class="col-8 fw-bold">Sub Total: </span>
-                        <span class="col-4">100</span>
-                        <div class="border my-2"></div>
-                        <span class="col-8 fw-bold">Tax: </span>
-                        <span class="col-4">15%</span>
-                        <div class="border my-2"></div>
-                        <span class="col-8 fw-bold">Total (SGD): </span>
-                        <span class="col-4">100</span>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </form>
     </div>
 @endsection

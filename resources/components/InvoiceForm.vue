9d2000b12a8a7888451c7a5143ed387b630fcc2d
Marcus Loo <marcuslooek@gmail.com>
2023-03-09 11:23:09 +0800

    invoices ui and controller

---
+++ resources/components/InvoiceForm.vue
@@ -0,0 +1,148 @@
+<template>
+    <div class="bg-white card shadow py-3 px-4">
+        <div class="row border-bottom mb-2">
+            <span class="h5">{{ $modelName }} Information</span>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-6 mb-2">
+                <label class="form-label">Client</label>
+                <!-- <input type="hidden" name="client_id" :value="client.id">
+                <multi-select v-model="client" track-by="id" label="id" :options="clients"></multi-select> -->
+            </div>
+            <div class="col-12 col-md-6 mb-2">
+                <label class="form-label">Date</label>
+                <input class="form-control" type="date" name="name" placeholder="date" value="{{ old('date') ?? (!empty($invoice) ? $invoice->date : '') }}" required>
+            </div>
+            <div class="col-12 col-md-6 mb-md-0 mb-2">
+                <label class="form-label">Client Address</label>
+                <input class="form-control" type="text" placeholder="Client Address" disabled>
+            </div>
+            <div class="col-12 col-md-6 mb-2">
+                <label class="form-label">Payment Term</label>
+                <input class="form-control" type="text" name="payment_term" placeholder="Payment Term" value="" required>
+            </div>
+            <div class="col-12 col-md-6 mb-2">
+                <label class="form-label">Sales Order (Optional)</label>
+                 <input type="hidden" name="sales_order_id" :value="salesOrder.id">
+                <multi-select v-model="salesOrder" track-by="id" label="id" :options="salesOrders"></multi-select>
+            </div>
+        </div>
+    </div>
+    <div class="border my-2 mb-3"></div>
+    <div class="bg-white card shadow py-3 px-4">
+        <div class="row mb-2">
+            <span class="h4">ADD PRODUCT</span>
+        </div>
+        <div class="overflow-auto">
+            <table class="table table-bordered text-center">
+                <thead>
+                    <tr>
+                        <th>Product Details</th>
+                        <th>Quantity</th>
+                        <th>Unit Price</th>
+                        <th>Sub Total</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(item, index) in products">
+                        <td>
+                            <input type="hidden" :name="'product[' + index + '][product_variant_id]'" :value="item.productVariant.id">
+                            <multi-select v-model="item.productVariant" track-by="name" label="name" :options="props.productVariants"></multi-select>
+                        </td>
+                        <td>
+                            <input class="form-control" type="number" min="0" :name="'product[' + index + '][quantity]'" v-model="item.quantity" @input="updateProductSubTotal(item)" required>
+                        </td>
+                        <td>{{ item.productVariant.unit_price }}</td>
+                        <td>{{ item.subTotal }}</td>
+                        <td>
+                            <span class="btn" @click="removeProduct(index)">
+                                <i class="uil-trash" style="color: red"></i>
+                            </span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="row container-fluid">
+            <div class="col-12 col-md-4 mb-md-0 mb-3">
+                <div class="row px-0">
+                    <span class="btn px-0 text-start text-primary text-decoration-underline" @click="addProducts()">Add another line</span>
+                    <div class="px-0">
+                        <label class="form-label px-0">Notes and instructions</label>
+                        <textarea class="form-control" placeholder="Notes and instructions" rows="5" style="resize: none"></textarea>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5"></div>
+            <div class="col-12 col-md-3">
+                <div class="row text-end">
+                    <span class="col-7 fw-bold my-auto">Sub Total: </span>
+                    <span class="col-5">{{ subTotal.toFixed(2) }}</span>
+                    <div class="border my-2"></div>
+                    <span class="col-7 fw-bold my-auto">Tax %: </span>
+                    <span class="col-5"><input class="form-control" type="number" v-model="taxRate" min="0" max="100" step="1" required></span>
+                    <div class="border my-2"></div>
+                    <span class="col-7 fw-bold my-auto">Total (SGD): </span>
+                    <span class="col-5">{{ grandTotal.toFixed(2) }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="row col-12">
+            <button type="submit" class="col-12 col-md-1 btn btn-success m-2">Create</button>
+            <a href="/admin/invoices" class="col-12 col-md-1 btn btn-dark m-2">Back</a>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineComponent, ref } from 'vue';
+
+let props = defineProps({
+    salesOrders: Array,
+    productBundles: Array,
+    productVariants: Array,
+    taxRate: Number,
+    clients: Array,
+});
+
+const subTotal = ref(0);
+const salesOrder = ref({});
+const client = ref({});
+const grandTotal = ref(0);
+const taxRate = ref(props.taxRate);
+const products = ref([{
+    'productVariant': '',
+    'quantity': 0,
+    'subTotal': 0,
+}]);
+
+function addProducts() {
+    this.products.push({
+        'productVariant': '',
+        'quantity': 0,
+        'subTotal': 0,
+    })
+}
+
+function removeProduct(index) {
+    this.products.splice(index, 1);
+    this.updateTotalPrice();
+}
+
+function updateProductSubTotal(item) {
+    item.subTotal = item.productVariant.unit_price * item.quantity;
+    this.updateTotalPrice();
+}
+
+function updateTotalPrice() {
+    this.subTotal = 0;
+    this.grandTotal = 0;
+    this.products.forEach(item => {
+        this.subTotal += item.subTotal;
+        this.grandTotal += item.subTotal;
+    });
+    this.grandTotal *= 1 + (props.taxRate / 100);
+}
+
+</script>
---
+++ resources/controllers/InvoiceController.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Invoice;
+use App\Models\Product;
+use App\Models\ProductVariant;
+use App\Models\SalesOrder;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Exception;
+use Vecapital\Vebase\Http\Controllers\VeController;
+use Illuminate\Support\Str;
+
+class InvoiceController extends VeController
+{
+   /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $salesOrder = SalesOrder::get(['id']);
+        // $productBundles = Product::get(['id', 'name']);
+        $productVariants = ProductVariant::get(['id', 'name']);
+
+        $compact = [
+            'salesOrders' => $salesOrder,
+            // 'productBundles' => $productBundles,
+            'productVariants' => $productVariants,
+            'taxRate' => 7,
+            'routeModel' => Str::singular($this->routeName),
+            'model' => $this->model,
+            'modelName' => $this->modelName,
+            'routeName' => $this->routeName,
+            'routePrefix' => $this->folder,
+        ];
+
+        return view('admin.invoices.create', $compact);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->input();
+
+        $validator = Validator::make($input, Invoice::class()->createValidator());
+
+        if ($validator->fails()) {
+            flash('Error: ' . implode(" ", $validator->errors()->all()))->error();
+            return back()->withInput($request->input())->withErrors($validator);
+        }
+
+        $client = User::find($input['user_id']);
+        if (!empty($input['sales_order_id'])) {
+            $salesOrder = SalesOrder::find($input['sales_order_id'])->with('orderItems');
+        }
+        
+        try {
+            DB::beginTransaction();
+
+            $invoice = Invoice::create($input + ['created_by' => Auth::id()]);
+
+            if (!empty($salesOrder) && $salesOrder->orderItems->isNotEmpty()) {
+                foreach ($salesOrder->orderItems as $orderItem) {
+                    $orderItem = InvoiceItem::create([
+                        'invoice_id' => $invoice->id,
+                        'product_id' => $orderItem->product_id, 
+                        'product_variant_id' => $orderItem->product_variant_id,
+                        'name' => $orderItem->productVariant->name, 
+                        'quantity' => $orderItem->quantity, 
+                        'unit_price' => $orderItem->unit_price, 
+                        'sub_total' => $orderItem->sub_total, 
+                    ]);
+                }
+            } else {
+                foreach ($input['products'] as $productVariant) {
+                    $productVariant = ProductVariant::find($productVariant);
+                    $orderItem = InvoiceItem::create([
+                        'invoice_id' => $invoice->id,
+                        'product_id' => $productVariant->product_id, 
+                        'product_variant_id' => $productVariant->id,
+                        'name' => $productVariant->name, 
+                        'quantity' => $orderItem->quantity, 
+                        'unit_price' => $productVariant->unit_price, 
+                        'sub_total' => $productVariant->unit_price * 1, 
+                    ]);
+                }
+            }
+
+            DB::commit();
+            flash()->success('Successfully created invoice');
+            return redirect()->route('admin.invoices.index');
+        } catch (Exception $exception) {
+            log()::error($exception);
+            DB::rollBack();
+            flash()->error('There was an issue creating invoice');
+            return back()->withInput();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Invoice  $invoice
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request, $invoice)
+    {
+        return view('admin.invoices.show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Invoice  $invoice
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Request $request, $invoice)
+    {
+        $salesOrder = SalesOrder::get(['id']);
+        // $productBundles = Product::get(['id', 'name']);
+        $productVariants = ProductVariant::get(['id', 'name']);
+
+        $compact = [
+            'salesOrders' => $salesOrder,
+            // 'productBundles' => $productBundles,
+            'productVariants' => $productVariants,
+            'taxRate' => 7,
+            'routeModel' => Str::singular($this->routeName),
+            'model' => $this->model,
+            'modelName' => $this->modelName,
+            'routeName' => $this->routeName,
+            'routePrefix' => $this->folder,
+        ];
+
+        return view('admin.invoices.edit', $compact);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Invoice  $invoice
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $invoice)
+    {
+        $input = $request->input();
+
+        $validator = Validator::make($input, $invoice->createValidator());
+
+        if ($validator->fails()) {
+            flash('Error: ' . implode(" ", $validator->errors()->all()))->error();
+            return back()->withInput($request->input())->withErrors($validator);
+        }
+
+        try {
+            DB::beginTransaction();
+
+
+
+            DB::commit();
+            flash()->success('Successfully updated invoice');
+            return redirect()->route('admin.invoices.index');
+        } catch (Exception $exception) {
+            log()::error($exception);
+            DB::rollBack();
+            flash()->error('There was an issue updating invoice');
+            return back()->withInput();
+        }
+    }
+}

91cd745b8d3f57ae1ec5a7324229823d0a237bd7
agung_kurniawan <agung_kurniawan@vecapital.asia>
2023-03-14 09:45:09 +0700

    move dashboard

--- resources/views/admin/dashboard.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts/layout')
-
-@section('content')
-    <dashboard
-        :monthly-orders="{{ json_encode($monthlyOrders) }}"
-        :purchase-orders="{{ json_encode($purchaseOrders) }}"
-        :sales-orders="{{ json_encode($salesOrders) }}"/>
-@endsection

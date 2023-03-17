1a754159ee5f2bc9413b0ce1e20f92b3a2361db2
agung_kurniawan <agung_kurniawan@vecapital.asia>
2023-03-17 13:42:44 +0700

    remove view from src

--- src/resources/views/admin/delivery-orders/form.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.layout')
-
-@section('content')
-    <admin-delivery-order-form 
-        action="{{ $action }}"
-        form-action="{{ $action == 'create' ? route('admin.delivery-orders.store') : route('admin.delivery-orders.update', ['delivery_order' => $deliveryOrder]) }}"
-        @if (!empty($deliveryOrder)) :delivery-order="{{ $deliveryOrder->toJson() }}" @endif/>
-@stop

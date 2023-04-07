4480062330f26237c34d0632fb32ea891239380a
Lutfi Jamaluddin <lutfi@vecapital.asia>
2023-04-07 09:38:31 +0800

    - validation fixes

--- resources/Imports/ClientImport.php
+++ resources/Imports/ClientImport.php
@@ -4,8 +4,10 @@ namespace App\Imports;
 
 use App\Models\Client;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
 
-class ClientImport implements ToModel
+class ClientImport implements ToModel, WithHeadingRow, WithValidation
 {
     /**
     * @param array $row
@@ -15,18 +17,34 @@ class ClientImport implements ToModel
     public function model(array $row)
     {
         return new Client([
-            'name' => $row[0],
-            'email' => $row[1],
-            'company_name' => $row[2],
-            'phone' => $row[3],
-            'address_1' => $row[4],
-            'address_2' => $row[5],
-            'city' => $row[6],
-            'state' => $row[7],
-            'postcode' => $row[8],
-            'country' => $row[9],
+            'name' => $row['name'],
+            'email' => $row['email'],
+            'company_name' => $row['company_name'],
+            'phone' => $row['phone'],
+            'address_1' => $row['address1'],
+            'address_2' => $row['address2'],
+            'city' => $row['city'],
+            'state' => $row['state'],
+            'postcode' => $row['postcode'],
+            'country' => $row['country'],
             'image' => null,
             'status' => Client::STATUS_ACTIVE,
         ]);
+    }
+
+    public function rules(): array
+    {
+        return [
+            '*.name' => 'required|string',
+            '*.email' => 'required|string',
+            '*.company_name' => 'nullable|string',
+            '*.phone' => 'nullable|numeric',
+            '*.address_1' => 'nullable|string',
+            '*.address_2' => 'nullable|string',
+            '*.city' => 'nullable|string',
+            '*.state' => 'nullable|string',
+            '*.postcode' => 'nullable|numeric',
+            '*.country' => 'required|string',
+        ];
     }
 }

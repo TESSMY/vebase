71d6b46840b37986cad5468d189d024f70a30885
Marcus Loo <marcuslooek@gmail.com>
2023-05-08 13:47:35 +0800

    swap position of search and display per page

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -24,7 +24,12 @@
             <form action="{{ route($routePrefix . '.' . $routeName . '.index') }}" method="GET" id="form">
                 @csrf
                 <div class="row mb-3">
-                    <div class="col-12 col-md-2 mb-3 mb-md-0 d-flex">
+                    <div class="col-12 col-md-2 mb-3 mb-md-0 d-md-flex">
+                        <label class="form-label my-auto me-md-2">Search: </label>
+                        <input class="form-control" type="search" placeholder="Search" name="search" value="{{ request()->input('search') }}">
+                    </div>
+                    <div class="col-md-8"></div>
+                    <div class="col-12 col-md-2 d-flex">
                         <span class="my-auto">Display:</span>
                         <select class="form-select mx-2" name="limit">
                             <option selected>10</option>
@@ -32,11 +37,6 @@
                             <option value="50">50</option>
                         </select>
                         <span class="my-auto">{{ $modelName }}</span>
-                    </div>
-                    <div class="col-md-8"></div>
-                    <div class="col-12 col-md-2 p-0 d-md-flex">
-                        <label class="form-label my-auto me-md-2">Search: </label>
-                        <input class="form-control" type="search" placeholder="Search" name="search" value="{{ request()->input('search') }}">
                     </div>
                 </div>
             </form>

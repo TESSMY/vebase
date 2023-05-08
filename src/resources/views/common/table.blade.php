fa3be1270bb5b51b6eeccb20b3be3ea5cad459ce
Marcus Loo <marcuslooek@gmail.com>
2023-05-08 14:18:41 +0800

    fix

--- src/resources/views/common/table.blade.php
+++ src/resources/views/common/table.blade.php
@@ -35,7 +35,7 @@
                         @elseif (strtolower($indexField['columnName']) == 'show_and_edit') 
                             <td>
                                 @can('view', $$routeModel)
-                                    <a class="me-4" href="{{ route($routePrefix . '.' . $routeName . '.show', $$routeModel->getRouteKey()) }}"><i class="uil-eye"></i></a>
+                                    <a class="me-3" href="{{ route($routePrefix . '.' . $routeName . '.show', $$routeModel->getRouteKey()) }}"><i class="uil-eye"></i></a>
                                 @endcan
                                 @can('update', $$routeModel)
                                     <a href="{{ route($routePrefix . '.' . $routeName . '.edit', $$routeModel->getRouteKey()) }}"><i class="uil-edit"></i></a>

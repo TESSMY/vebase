d622ca122f5af18664cf360111ff9c4d6fe038c7
Marcus loo <Marcuslooek@gmail.com>
2026-01-02 12:12:23 +0800

    no message

--- src/resources/views/common/table.blade.php
+++ src/resources/views/common/table.blade.php
@@ -85,7 +85,7 @@
                             }
                         @endphp
                         @if (View::exists($routePrefix . '.' . $routeName . '.index.' . $columnName))
-                            @include($routePrefix . '.' . $routeName . '.index.' . $columnName, ['data' => $$routeModel])
+                            @include($routePrefix . '.' . $routeName . '.index.' . $columnName . '-th', [$routeModel => $model])
                         @elseif ($columnName == 'show')
                             @can('view', $$routeModel)
                                 <td><a href="{{ route($routePrefix . '.' . $routeName . '.show', $$routeModel->getRouteKey()) }}"><i class="uil-eye"></i></a></td>
